1bb93ff3200f292552e87476ecd61742a35fad56
wirapramuja <wiraapramuja44@gmail.com>
2023-06-02 05:41:58 +0700

    minglemusic

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    images: {
+      domains: ["misc.scdn.co", "i.scdn.co", "geo-media.beatsource.com", "i1.sndcdn.com", "media.pitchfork.com", "seed-mix-image.spotifycdn.com", "tbvswgcbrilqlaxdsxii.supabase.co",
+      "zdpvbttrclzsvobmhlij.supabase.co"
+    ]
+    }
+  }
+  
+  module.exports = nextConfig
